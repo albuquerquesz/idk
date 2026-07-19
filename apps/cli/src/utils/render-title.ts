658fbb9d124f001d3b76abb8f0b2b10ba7bfedfb
Victor Albuquerque <albq.victor@gmail.com>
2026-07-19 09:18:01 -0300

    feat(cli): render title with figlet

--- apps/cli/src/utils/render-title.ts
+++ apps/cli/src/utils/render-title.ts
@@ -1,12 +1,6 @@
 import gradient from "gradient-string";
 
-export const TITLE_TEXT = `
- IIIII        DDDD     OOO    N   N  TTTTT        K   K  N   N   OOO   W   W
-   I          D   D   O   O   NN  N    T          K  K   NN  N  O   O  W   W
-   I          D   D   O   O   N N N    T          KKK    N N N  O   O  W W W
-   I          D   D   O   O   N  NN    T          K  K   N  NN  O   O  WW WW
- IIIII        DDDD     OOO    N   N    T          K   K  N   N   OOO   W   W
- `;
+import { renderAsciiTitle } from "./ascii-handler";
 
 const catppuccinTheme = {
   pink: "#F5C2E7",
@@ -24,13 +18,14 @@ const catppuccinTheme = {
 
 export const renderTitle = () => {
   const terminalWidth = process.stdout.columns || 80;
-  const titleLines = TITLE_TEXT.split("\n");
+  const title = renderAsciiTitle();
+  const titleLines = title.split("\n");
   const titleWidth = Math.max(...titleLines.map((line) => line.length));
 
   if (terminalWidth < titleWidth) {
     const simplifiedTitle = `I dont know`;
     console.log(gradient(Object.values(catppuccinTheme)).multiline(simplifiedTitle));
   } else {
-    console.log(gradient(Object.values(catppuccinTheme)).multiline(TITLE_TEXT));
+    console.log(gradient(Object.values(catppuccinTheme)).multiline(title));
   }
 };
